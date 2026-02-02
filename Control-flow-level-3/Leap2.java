6a7502ac5db851b23fd1be152544cb556a815130
khushi-saini-23 <khushisainimtr@gmail.com>
2026-02-02 12:25:09 +0530

    [Khushi] recoverfile

--- Control-flow-level-3/Leap2.java
+++ Control-flow-level-3/Leap2.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Leap2{
 	public static void main(String[]args){
 		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the year");
 		int year=sc.nextInt();
 		if(year%4==0 || year%400==0){
 			System.out.println("its leap year");
