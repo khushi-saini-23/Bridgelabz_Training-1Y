b54c28b276aca0e879c6739dd35e72b0d1566d66
khushi-saini-23 <khushisainimtr@gmail.com>
2026-03-07 21:30:04 +0530

    Add copy constructor to Person class

--- 07-ClassAndObject/Level-2/Person.java
+++ 07-ClassAndObject/Level-2/Person.java
@@ -5,15 +5,18 @@ public class Person{
 		name=n;
 		age=a;
 	}
+	Person(Person p) {
+        name = p.name;
+        age = p.age;
+    }
 	public void show(){
 		System.out.println("Name:"+name);
 		System.out.println("age:"+age);
 	}
 	public static void main(String[]args){
-		Person p1=new Person("Khushi",18);
-		Person p2=new Person(p1);
+		Person p1 = new Person("Akshara",18);
+		Person p2 = new Person(p1);
 		p1.show();
 		p2.show();
 	}
 }
-		
