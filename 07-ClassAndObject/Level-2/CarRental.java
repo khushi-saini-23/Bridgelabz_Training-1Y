75dc6b7c91ad79204bdb03795b31767defdf3850
khushi-saini-23 <khushisainimtr@gmail.com>
2026-03-07 21:28:40 +0530

    Refactor CarRental class and update constructor usage

--- 07-ClassAndObject/Level-2/CarRental.java
+++ 07-ClassAndObject/Level-2/CarRental.java
@@ -1,36 +1,37 @@
-class CarRental{
-	String customerName;
-	String carmodel;
-	int rentaldays;
-	CarRental(){
-		customerName="Customer";
-		carmodel="basic";
-		rental=1;
-	}
-	CarRental(String n, String m,String d){
-		customerName=n;
-		carmodel=m;
-		rentaldays=d;
-	}
-	int totalCost() {
+class CarRental {
+
+    String customerName;
+    String carmodel;
+    int rentaldays;
+	
+    CarRental() {
+        customerName = "Customer";
+        carmodel = "Basic";
+        rentaldays = 1;
+    }
+    CarRental(String n, String m, int d) {
+        customerName = n;
+        carmodel = m;
+        rentaldays = d;
+    }
+    int totalCost() {
         int pricePerDay = 500;
         return rentaldays * pricePerDay;
     }
-
     void show() {
         System.out.println("Customer Name: " + customerName);
         System.out.println("Car Model: " + carmodel);
         System.out.println("Rental Days: " + rentaldays);
         System.out.println("Total Cost: " + totalCost());
+        System.out.println();
     }
 
     public static void main(String[] args) {
 
         CarRental r1 = new CarRental();
-        CarRental r2 = new CarRental("Khushi", "Swift", 3);
+        CarRental r2 = new CarRental("Akshara", "BMW", 3);
 
         r1.show();
         r2.show();
     }
 }
-	
