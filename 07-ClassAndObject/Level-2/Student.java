effb8e137cf5c1d4317b89cc1fd2ea2c72324b5d
khushi-saini-23 <khushisainimtr@gmail.com>
2026-03-07 21:48:59 +0530

    Refactor Student class and update PostgraduateStudent

--- 07-ClassAndObject/Level-2/Student.java
+++ 07-ClassAndObject/Level-2/Student.java
@@ -1,4 +1,4 @@
-class Student {
+public class Student {
 
     public int roll;
     protected String name;
@@ -8,29 +8,27 @@ class Student {
         cgpa = c;
     }
 
-    double getCgpa() {
+    public double getCgpa() {
         return cgpa;
     }
-}
 
-class PostgraduateStudent extends Student {
-
-    void show() {
-        System.out.println("Roll: " + roll);
-        System.out.println("Name: " + name);
-        System.out.println("CGPA: " + getCgpa());
-    }
-}
-
-class Main {
     public static void main(String[] args) {
 
         PostgraduateStudent s = new PostgraduateStudent();
 
         s.roll = 37;
-        s.name = "khushi";
+        s.name = "Khushi";
         s.setCgpa(8.5);
 
         s.show();
+    }
+}
+
+class PostgraduateStudent extends Student {
+
+    public void show() {
+        System.out.println("Roll: " + roll);
+        System.out.println("Name: " + name);
+        System.out.println("CGPA: " + getCgpa());
     }
 }
